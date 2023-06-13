44a42b47c59164a73521b7729f19cafc7b24264d
Daniel Gary <daniel@gatewayapps.com>
2023-06-13 17:09:56 -0500

    with session

--- app/auth/[...nextauth]/route.ts
+++ app/auth/[...nextauth]/route.ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma) as any,
   providers: [
     EmailProvider({
       server: process.env.MAIL_SERVER,
